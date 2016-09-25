d943ebb52a8e5c4c111d5d74ed5cc08c0517914b
elh <reo.retriever@gmail.com>
2016-09-26 02:12:17 +0900

    Test motion Tracking

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,6 +5,7 @@
 #include "utils/utils.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/tracking/tracker.hpp"
 
 int main(int argc, const char* argv[]) {
 	auto args = argv_to_vec(argc, argv);
@@ -25,10 +26,44 @@ int main(int argc, const char* argv[]) {
 	} else std::cout << "succeeded\n";
 
 	cv::namedWindow("main");
+
 	cv::Mat frame;
+	video >> frame;
+
+	auto color1 = cv::Scalar(0, 255, 255);
+	auto color2 = cv::Scalar(255, 0, 255);
+	auto color3 = cv::Scalar(255, 255, 0);
+
+	auto roi1 = cv::selectROI("main", frame);
+	auto roi2 = cv::selectROI("main", frame);
+	auto roi3 = cv::selectROI("main", frame);
+
+	cv::Ptr<cv::Tracker> tracker1 = cv::Tracker::create("MIL");
+	cv::Ptr<cv::Tracker> tracker2 = cv::Tracker::create("MIL");
+	cv::Ptr<cv::Tracker> tracker3 = cv::Tracker::create("MIL");
+
+	tracker1->init(frame, roi1);
+	tracker2->init(frame, roi2);
+	tracker3->init(frame, roi3);
+
+	double fps = video.get(CV_CAP_PROP_FPS);
+    cv::Size size = cv::Size(video.get(CV_CAP_PROP_FRAME_WIDTH), video.get(CV_CAP_PROP_FRAME_HEIGHT));
+    const int fourcc = cv::VideoWriter::fourcc('X', 'V', 'I', 'D');
+    std::string filename = "output.avi";
+    cv::VideoWriter writer(filename, fourcc, fps, size);
+
 	while(video.grab()) {
-		video.retrieve(frame);
+		tracker1->update(frame, roi1);
+		tracker2->update(frame, roi2);
+		tracker3->update(frame, roi3);
+
+		cv::rectangle(frame, roi1, color1, 1, 1);
+		cv::rectangle(frame, roi2, color2, 1, 1);
+		cv::rectangle(frame, roi3, color3, 1, 1);
+
 		cv::imshow("main", frame);
-		cv::waitKey();
+		writer << frame;
+
+		video.retrieve(frame);
 	}
 }
