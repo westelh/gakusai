3e6f07c6e96d3f041e630af479eb717ff253b01a
elh <reo.retriever@gmail.com>
2016-09-25 11:55:26 +0900

    Support OpenCV
    -Add cmake required library OpenCV
    -Add script cleans build dir

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,1 +1,11 @@
-int main() {}
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+int main() {
+	cv::Mat mat;
+	mat = cv::imread("s", 0);
+	if (mat.empty()) {
+		std::cout << "reading file failed\n";
+		return -1;
+	}
+}
